57e2712e29f65f9544b9c1011926edd91a82641a
alhassan <121292207+alhasasn@users.noreply.github.com>
2023-12-20 20:59:11 +0100

    made it cleaner

--- rps.py
+++ rps.py
@@ -1,7 +1,5 @@
 import random
 choices=['rock','paper','scissors']
-play_string='Your choice was {} '
-computer_string='The computers choice was {} '
 while True:
     player_chioce= input("pick a choice rock,paper,scissors  ").lower()
     computer_choice = random.choice(choices)
@@ -11,33 +9,33 @@ while True:
     if player_chioce.lower() in ['exit', 'quit']:
        break
     if player_chioce==computer_choice:
-        print(play_string.format(player_chioce) )
-        print(computer_string.format(computer_choice))
+        print(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('TIE')
     elif player_chioce== 'rock' and computer_choice=='scissors' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        print(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('you win')
     elif player_chioce== 'rock' and computer_choice=='paper' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        prin(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('You lose')
     elif player_chioce== 'scissors' and computer_choice=='rock' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        print(f'your choice was{ player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('You lose')
     elif player_chioce== 'scissors' and computer_choice=='paper' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        print(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('You WIN')
     elif player_chioce== 'paper' and computer_choice=='scissors' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        print(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('You lose')
     elif player_chioce== 'paper' and computer_choice=='rock' :
-        print(play_string.format(player_chioce))
-        print(computer_string.format(computer_choice))
+        print(f'your choice was {player_chioce}' )
+        print(f'The computers choice was {computer_choice}')
         print('You Win')
-        
+            
 print('THANK YOU FOR THE GAME')    
 
