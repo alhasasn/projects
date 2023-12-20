cf9bb7af6920c064dce20801b81d196bb03ed1dc
alhassan <121292207+alhasasn@users.noreply.github.com>
2023-12-20 18:30:41 +0100

    Update todolist_2.py

--- todolist_2.py
+++ todolist_2.py
@@ -2,19 +2,19 @@ todolist =[]
 modifiers=['k','r']
 while True :
     item=input('enter a list item or enter \'k\' to see list items and types \'r\' to remove an item ').lower()
-    if item in modifiers and item=='k':
+    if item in modifiers and item=='k': #Displays list
         for item in todolist:
             print(item)
-    elif item in modifiers and item =='r':
+    elif item in modifiers and item =='r': # REMOVES ITEMS IN LIST
         try:
              for item in todolist:
                   print(item)
              remove_item=input("Type the item_____")
-             todolist.remove(remove_item)
+             todolist.remove(remove_item) 
         except:
             print('Item not in list')       
     elif item == 'exit':
-        for item in todolist:
+        for item in todolist: #TO ESCAPE THE LOOP
             print(item)
         break
     else:
